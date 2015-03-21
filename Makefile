96300d7e9adbe5bc4e04f2e22fc609539e89e754
Richard H Lee <ricardohenrylee@gmail.com>
2015-03-21 22:48:05 +0000

    set only networks facing interface to promiscuous

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc hub.c -Wall -Wextra -o hub
+	gcc hub.c -Wall -Wextra -Werror -pedantic -o hub
--- hub.c
+++ hub.c
@@ -10,87 +10,77 @@
 #include <net/if.h>
 
 
-#define USAGE "Usage: hub [-d] ifname0 ifname1\n"
+#define USAGE "Usage: hub [-d] network-if client-if\n"
 #define BUFFER_SIZE 2048
 
 
-void setPromiscuous(const int socketFd, const char *interfaceName);
 void setupAddress(struct sockaddr_ll *address, const int index);
 
 
 void exitMessage(const int exitCode,  const char *format, ...);
 
-// TODO: Close fd's
+/* TODO: Close fd's */
 
 int
 main(const int argc, const char **argv) {
-  const char *interface0Name, *interface1Name;
-  int socketFd, interface0Index, interface1Index, bytesRead;
-  struct sockaddr_ll interface0Address, interface1Address, packetAddress;
+  const char *networkInterfaceName, *clientInterfaceName;
+  int socketFd, networkInterfaceIndex, clientInterfaceIndex, bytesRead;
+  struct sockaddr_ll networkInterfaceAddress, clientInterfaceAddress, packetAddress;
   uint8_t buffer[BUFFER_SIZE];
   socklen_t packetAddressLen = sizeof(packetAddress);
+  struct ifreq ifreq;
   
   if(!((argc == 3) || ((argc == 4) && strncmp(argv[1], "-d", 3)))) {
     printf(USAGE);
     exit(1);
   }
-  interface0Name = argv[argc - 2];
-  interface1Name = argv[argc - 1];
+  networkInterfaceName = argv[argc - 2];
+  clientInterfaceName = argv[argc - 1];
 
-  // Check interface names and get indices
-  if(!(interface0Index = if_nametoindex(interface0Name)))
-    exitMessage(1, "Error: Interface %s does not exist", interface0Name);
-  if(!(interface1Index = if_nametoindex(interface1Name)))
-    exitMessage(1, "Error: Interface %s does not exist", interface1Name);
+  /* Check interface names and get indices */
+  if(!(networkInterfaceIndex = if_nametoindex(networkInterfaceName)))
+    exitMessage(1, "Error: Interface %s does not exist", networkInterfaceName);
+  if(!(clientInterfaceIndex = if_nametoindex(clientInterfaceName)))
+    exitMessage(1, "Error: Interface %s does not exist", clientInterfaceName);
 
-  // Create socket
+  /* Create socket */
   if((socketFd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1)
     exitMessage(1, "Error: Could not create socket");
 
-  setPromiscuous(socketFd, interface0Name);
-  setPromiscuous(socketFd, interface1Name);
-
-  setupAddress(&interface0Address, interface0Index);
-  setupAddress(&interface1Address, interface1Index);
-  
-  // Repeater loop
-  while((bytesRead =
-      recvfrom(socketFd, buffer, BUFFER_SIZE, 0,
-         (struct sockaddr*) &packetAddress, &packetAddressLen)))
-    if(packetAddress.sll_pkttype != 4) {
-      if(packetAddress.sll_ifindex == interface0Index)
-        if(sendto(socketFd, buffer, bytesRead, 0,
-              (struct sockaddr*) &interface1Address, sizeof(struct sockaddr_ll)) !=
-            bytesRead)
-          exitMessage(1, "Error: Could not send data");
-      if(packetAddress.sll_ifindex == interface1Index)
-        if(sendto(socketFd, buffer, bytesRead, 0,
-              (struct sockaddr*) &interface0Address, sizeof(struct sockaddr_ll)) !=
-            bytesRead)
-          exitMessage(1, "Error: Could not send data");
-    }
-
-  return 0;
-}
-
-
-void
-setPromiscuous(const int socketFd, const char *interfaceName) {
-  struct ifreq ifreq;
-
-  strncpy(ifreq.ifr_name, interfaceName, IFNAMSIZ - 1);
+  /* Put client interface into promiscuous mode */
+  strncpy(ifreq.ifr_name, clientInterfaceName, IFNAMSIZ - 1);
   if(ioctl(socketFd, SIOCGIFFLAGS, &ifreq))
     exitMessage(1, "Error: Could not execute interface reuqest");
   ifreq.ifr_flags |= IFF_PROMISC;
   if(ioctl(socketFd, SIOCSIFFLAGS, &ifreq))
     exitMessage(1, "Error: Could not execute interface reuqest");
-}
 
-void
-setupAddress(struct sockaddr_ll *address, const int index) {
-  memset(address, 0, sizeof(struct sockaddr_ll));
-  address->sll_ifindex = index;
-  address->sll_halen = ETH_ALEN;
+  /* Set outgoing interface indices and address length */
+  memset(&networkInterfaceAddress, 0, sizeof(struct sockaddr_ll));
+  networkInterfaceAddress.sll_ifindex = networkInterfaceIndex;
+  networkInterfaceAddress.sll_halen = ETH_ALEN;
+  memset(&clientInterfaceAddress, 0, sizeof(struct sockaddr_ll));
+  clientInterfaceAddress.sll_ifindex = clientInterfaceIndex;
+  clientInterfaceAddress.sll_halen = ETH_ALEN;
+  
+  /* Repeater loop */
+  while((bytesRead =
+      recvfrom(socketFd, buffer, BUFFER_SIZE, 0,
+         (struct sockaddr*) &packetAddress, &packetAddressLen)))
+    if(packetAddress.sll_pkttype != 4) {
+      if(packetAddress.sll_ifindex == networkInterfaceIndex)
+        if(sendto(socketFd, buffer, bytesRead, 0,
+              (struct sockaddr*) &clientInterfaceAddress, sizeof(struct sockaddr_ll)) !=
+            bytesRead)
+          exitMessage(1, "Error: Could not send data");
+      if(packetAddress.sll_ifindex == clientInterfaceIndex)
+        if(sendto(socketFd, buffer, bytesRead, 0,
+              (struct sockaddr*) &networkInterfaceAddress, sizeof(struct sockaddr_ll)) !=
+            bytesRead)
+          exitMessage(1, "Error: Could not send data");
+    }
+
+  return 0;
 }
 
 
